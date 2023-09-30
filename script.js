a1a843afeceba9bfa36bb4148eebea777f38cf50
sanyam <sanyampatil869@gmail.com>
2023-09-30 23:06:09 +0530

    created code

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 // console.log('hii');
- let btn=document.querySelector(".btn");
-  let Name=document.querySelector("#name");
-  let address=document.querySelector("#address");
-  let TenthPerage=document.querySelector("#TenthPerage");
-  let  twethPerage=document.querySelector('#twethPerage')
+let btn = document.querySelector(".btn");
+let Name = document.querySelector("#name");
+let address = document.querySelector("#address");
+let TenthPerage = document.querySelector("#TenthPerage");
+let twethPerage = document.querySelector('#twethPerage')
+let form1 = document.querySelector('#from1')
+let Mobilenumber = document.querySelector('#Mobilenumber')
+// var tabLE=document.getElementsByTagName("table")
+var Table_row = document.querySelector("table")
+// console.log(tabLE)
 
-let Mobilenumber=document.querySelector('#Mobilenumber')
-  // var tabLE=document.getElementsByTagName("table")
-  var Table_row=document.querySelector(".Table-row")
-  // console.log(tabLE)
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
 
-btn.addEventListener("click",function(){
 
-  let CreateTable=` <td>${Name.value}</td>
+  let CreateTable =`
+  
+  <tr>
+  <td>${Name.value}</td>
   <td>${address.value}</td>
   <td>${TenthPerage.value}</td>
   <td>${twethPerage.value}</td>
   <td>${Mobilenumber.value}</td>
-`
+  </tr>`
 
-Table_row.innerHTML=CreateTable
-  
+  Table_row.innerHTML = Table_row.innerHTML + CreateTable;
+
+
+  console.log("jjjj")
+
 
 })
